8cbd4f0063aa3819e116d5663f53762c2d67626b
tthugy <supermanace7@gmail.com>
2022-07-08 21:59:01 +0900

    update

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,12 @@ import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 export default function Home() {
+  const scene = new THREE.Scene();
+  const controls = new OrbitControls( camera, renderer.domElement ); // example OrbitControls Components
   return (
     <div>
     </div>
