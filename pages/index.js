621174b5b117320ec6ba6cb11086b87cb5a001f1
tthugy <supermanace7@gmail.com>
2022-10-06 03:28:41 +0900

    update

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,21 @@
 import React from 'react';
-import * as THREE from 'three';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 
-class Viewer extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  componentDidMount() {
-    const width = window.innerWidth - 1;
-    const height = window.innerHeight - 1;
-
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(90, width / height, 1, 1000);
-
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(width, height);
-
-    this.element.appendChild(renderer.domElement);
-
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x99ff99 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-
-    camera.position.z = 5;
-
-    this.scene = scene;
-    this.camera = camera;
-    this.renderer = renderer;
-    this.cube = cube;
-    this.animate();
-  }
-
-  animate = () => {
-    this.renderer.render(this.scene, this.camera);
-    this.cube.rotation.x += 0.08;
-    this.cube.rotation.y += 0.08;
-    requestAnimationFrame(this.animate);
-  }
-
-  render() {
-    return (
-      <div ref={el => this.element = el} style={{ width: '100%', height: '100%', border: '.5px solid red' }} />
-    );
-  }
+function App() {
+  return (
+    <>
+      <Canvas>
+        <OrbitControls autoRotate={true}/>
+        <mesh>
+          <ambientLight intensity={1} />
+          <directionalLight position={[-1,0,1]} intensity={1.5} />
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial attach="material" color={0xa3b18a}/>
+        </mesh>
+      </Canvas>
+    </> 
+  );
 }
 
-export default Viewer;
+export default App;
